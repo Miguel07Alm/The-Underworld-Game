a430a08ac955489025738789f4c4b2a69cd38420
Miguel07Alm <miguelangelsisi098@gmail.com>
2020-07-20 14:49:08 +0200

    Le meti cosas bien calientes perra

--- TheGod.py
+++ TheGod.py
@@ -39,10 +39,12 @@ def blitRotateCenter(surf, image, topleft, angle):
 
     surf.blit(rotated_image, new_rect.topleft)
 
-def draw_window(win, dios, dangling, enemies,score,fps, surface1,surface2,evento):
+def draw_window(win, dios, danglings, enemies,score,fps, surface1,surface2,evento):
     win.blit(bg_img, (0,0))
     for enemigo in enemies:
         enemigo.draw(win)
+    for dang in danglings:
+        dang.draw(win)
     score_label = pygame.font.SysFont("arial", 50).render("Puntuacion: " + str(score),1,(255,255,255))
     win.blit(score_label, (win_width - score_label.get_width() - 15, 10))
     fps_label = pygame.font.SysFont("arial", 50).render("FPS: "+ str(fps), True, (255,255,255))
@@ -50,7 +52,6 @@ def draw_window(win, dios, dangling, enemies,score,fps, surface1,surface2,evento
     surface1.draw(win)
     surface2.draw(win)
     dios.draw(win,evento)
-    dangling.draw(win)
     pygame.display.flip()
 
     
@@ -61,7 +62,7 @@ class God():
     """
     max_rotation = 25
     imgs = god_imgs
-    rot_vel = 20
+    rot_vel = 15
     ulti_imgs = ultimates_imgs
     def __init__(self,x,y):
         self.x = x
@@ -103,6 +104,7 @@ class God():
             if self.inclinar > -90:
                 self.inclinar -= self.rot_vel
     def draw(self, win,evento):
+        global tiempo
         self.elapsed = pygame.time.get_ticks() - tiempo
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_SPACE:
@@ -128,51 +130,27 @@ class Dangling():
     max_rotation = 25
     img = dang_img
     rot_vel = 20
-    
-    def __init__(self,x,y):
+    def __init__(self,x):
         self.x = x
-        self.y = y
         self.inclinar = 0 #Grados a inclinarse
         self.tick_count = 0
         self.vel = 0
-        self.height = self.y
-    def fly(self):
-        self.vel = -10.5
-        self.tick_count = 0
-        self.height = self.y
-    def move(self,evento):
-        self.tick_count += 1
-        #Formula del desplazamiento (fisica)
-        displacement = self.vel*(self.tick_count) + 0.5*(3)*(self.tick_count)**2
-        # Vel. final
-        if displacement >= 16:
-            displacement = (displacement/abs(displacement)) * 16
-        if displacement < 0:
-            displacement -= 2
-
-        self.y = self.y + displacement
-        if evento.type == pygame.KEYUP:                
-            if evento.key == pygame.K_UP:
-                if displacement <  0 or self.y < self.height + 50:
-                    if self.inclinar < -90:
-                        self.inclinar += self.rot_vel*2
-        if displacement < 0 or self.y < self.height + 50:
-                    if self.inclinar < self.max_rotation:
-                        self.inclinar = self.max_rotation/4
-        else: 
-            if self.inclinar > -90:
-                self.inclinar -= self.rot_vel
-    def draw(self, win):
-        #Inclina la imagen
-        blitRotateCenter(win, self.img, (self.x,self.y), self.inclinar)
-        
+        self.cruzar = False
+        self.espacio = 600
+        self.configuracion()
+    def configuracion(self):
+        self.height = random.randrange(100,450) 
+    def move(self):
+        self.vel = 10
+        self.x = self.x - self.vel
+    def draw(self,win):
+        win.blit(self.img, (self.x, self.height))
     def get_mask(self):
         """
         consigue una mascara para la imagen actual del Dangling
         
         
         """
-        
         return pygame.mask.from_surface(self.img)
 
 class Enemies():
@@ -288,12 +266,11 @@ class Surface2:
 
 
 
-
 dios = God(100,300)
-dang = Dangling(500, 300)
+dangs = [Dangling(500)]
 superficie = Surface1(suelo)
 nubes = Surface2(tamaño)
-enemigos = [Enemies(700)]     
+enemigos = [Enemies(win_width)]     
 run = True  
 def main():    
     global WIN,run
@@ -309,31 +286,45 @@ def main():
         if keys[pygame.K_ESCAPE]:
             run = False
         if keys[pygame.K_SPACE]:
-    
             dios.fly()
-        if keys[pygame.K_UP]:
-            dang.fly()
-        rem = []
+        rem1 = []
+        rem2 = []
         add_enemigo = False
+        add_dang = False
         for enemigo in enemigos:
             enemigo.move()
+            # Si el dios choca contra el enemigo hace que se muera
             if enemigo.colision(dios,win):
                 run = False
-                
+            if enemigo.x + enemigo.enemy_top.get_width() < 0:
+                rem1.append(enemigo)
             if not enemigo.passed and enemigo.x < dios.x:
                 enemigo.passed = True
                 add_enemigo = True
+        for dang in dangs:
+            dang.move()
+            if dang.x < 0:
+                rem2.append(dang)
+            if not dang.cruzar and dang.x < dios.x:
+                dang.cruzar = True
+                add_dang = True            
+        if add_dang:
+            dangs.append(Dangling(win_width))
+        for r in rem2:
+            dangs.remove(r)
+        
+        
         if add_enemigo:
             score += 1
             enemigos.append(Enemies(win_width)) 
-        for r in rem:
+        for r in rem1:
             enemigos.remove(r)
         if dios.y  >= suelo or dios.y < -50:
             run = False
         dios.move(evento)
-        dang.move(evento) 
         nubes.move()
         superficie.move()
-        draw_window(win, dios, dang,enemigos,score, round(reloj.get_fps()), superficie,nubes,evento)                  
+        draw_window(win, dios, dangs,enemigos,score, round(reloj.get_fps()), superficie,nubes,evento)       
+                    
 if __name__ == '__main__':
     main()
